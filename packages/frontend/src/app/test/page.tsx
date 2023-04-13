5fe5eff246c3bf8d9e0d6e40cb25ee2d9b13d686
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-04-13 16:30:28 +0800

    add env

---
+++ packages/frontend/src/app/test/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAuthKit } from '@safe-global/auth-kit';
+type Props = {}
+
+const index = (props: Props) => {
+    return (
+        <div>
+            <button>Connect to google</button>
+
+        </div>
+    )
+}
+
+export default index
