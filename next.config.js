cf7ad52c87134a4fb5f018eb79936f9974df8aa3
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-04-16 04:52:32 +0900

    add build logic

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
